dc0c09fd81463ee97885698413416c09f1e9eaff
I578071 <suvabrata.chowdhury@sap.com>
2025-07-17 23:27:55 +0530

    Testing the screen and ignoring the application

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,3 +1,8 @@
+import com.tgraph.renderer.Screen;
+import com.tgraph.renderer.exception.ScreenDefinitionException;
+import com.tgraph.renderer.impl.terminal.TerminalScreen;
+import com.tgraph.scene.shape2d.Point2D;
+import com.tgraph.scene.shape2d.box.Box;
 
 /**
  * A dummy graphics application used for testing.
@@ -5,7 +10,12 @@
  * @author Nobody
  */
 public class Application {
-    public static void main (String[] args) {
-        System.out.println("Hello World");
+    public static void main (String[] args) throws ScreenDefinitionException {
+        TerminalScreen screen = TerminalScreen.getInstance(20,20);
+        
+        Box box = new Box(new Point2D(0, 0), new Point2D(10, 10));
+
+        screen.render(box);
+        screen.flush();
     }
 }
